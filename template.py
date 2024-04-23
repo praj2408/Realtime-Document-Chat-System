1cbf61a80c858a4e2b165b43aee37e28e8a9dea5
Prajwal Krishna <prajwalgbdr03@gmail.com>
2024-04-22 14:30:58 -1200

    made changes to template

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "textSummarizer"
+project_name = "Realtime-Document-Chat-System"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
